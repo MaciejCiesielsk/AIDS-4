5bec8c945b42fad22a43fe3867e880b7750fb913
MaciejCiesielsk <maciej182.mc@gmail.com>
2024-06-04 12:13:45 +0200

    dodaje mega

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-gowno mega
+import sys
+import os 
+import argparse 
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-hamilton", action="store_true" , help="Generate Hamilton graph")
+parser.add_argument("-non-hamilton", action="store_true" , help="Generate non hamilton graph")
+args = parser.parse_args()
